b1751d35226a0a31820bc7af95be1b31135368bf
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2021-02-17 14:39:25 +0300

    Update utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,5 @@
+import goblin from '../img/js.png';
+
 export function randomInteger(min, max) {
   const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
@@ -5,7 +7,7 @@ export function randomInteger(min, max) {
 
 export function createHtmlView() {
   const htmlView = document.createElement('img');
-  htmlView.setAttribute('src', 'src/img/js.png');
+  htmlView.setAttribute('src', goblin);
   htmlView.setAttribute('class', 'person');
   return htmlView;
 }
